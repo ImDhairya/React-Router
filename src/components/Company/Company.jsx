bb92f4509d86ff133a5be8dc446d100b5b5a713e
Dhairya <pandyadhairya789@gmail.com>
2023-12-12 10:39:40 +0530

    React router

---
+++ src/components/Company/Company.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { useParams } from "react-router-dom";
+
+function Company() {
+  return (
+    <div>sdfjsdjklfs df 
+      Lorem ipsumgdf dolov
+      Compahny  class
+      
+    </div>
+  )
+}
+
+export default Company
---
+++ src/components/ContactCompanyLayout.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { Outlet } from 'react-router-dom' 
+
+function ContactCompanyLayout() {
+  return (
+    <div>ContactCompanyLayout</div>
+  )
+}
+
+export default ContactCompanyLayout
